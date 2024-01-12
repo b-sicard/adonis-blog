6f192f86c0992c11699d8095962e51e8d8761ee4
bsicard <bsicard@waya-tech.com>
2024-01-12 22:29:58 +0100

    WIP User info

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,17 +1,24 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class AuthController {
 
     public async login({ auth, request, response }: HttpContextContract) {
 
-        const email = request.input('email')
-        const password = request.input('password')
+        const { email, password } = request.all()
 
         try {
+            const user = await User.findBy('email', email)
             const token = await auth.use('api').attempt(email, password)
-            return token
+            return response.send({
+                user,
+                token
+            })
         } catch {
-            return response.unauthorized('Invalid credentials')
+            return response.unauthorized({
+                title: 'Invalid credentials',
+                description: 'Invalid e-mail or password.'
+            })
         }
     }
 }
