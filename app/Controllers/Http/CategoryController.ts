af7774334bfaf0582d251e93c722a6fc80058f0e
bsicard <bs@benjaminsicard.com>
2024-01-23 20:30:22 +0100

    Categories list

--- app/Controllers/Http/CategoryController.ts
+++ app/Controllers/Http/CategoryController.ts
@@ -1,17 +1,18 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Category from 'App/Models/Category'
 
 export default class CategoryController {
 
-    public async list() {
-
+    public async list({ response }: HttpContextContract) {
+        const categories: Category[] = await Category.query().orderBy('created_at', 'desc')
+        console.log(categories)
+        return response.ok(categories)
     }
 
-    public async get() {
-
-    }
-
-    public async create() {
-
+    public async create({ request, response }: HttpContextContract) {
+        const category = await Category.create(request.all())
+        console.log(category)
+        return response.created(category)
     }
 
     public async update() {
